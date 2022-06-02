2b70bb0137aedbe03e134aab06b15f924b122b5f
stitou-nada <nadastito1087@gmail.com>
2022-06-02 12:35:30 +0000

    edit header

--- prototype-2/resources/views/pages/layouts/header.blade.php
+++ prototype-2/resources/views/pages/layouts/header.blade.php
@@ -6,8 +6,8 @@
     <!-- Navbar Search-->
     <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
         <div class="input-group">
-            <input class="form-control" type="text" placeholder="Search for..." aria-label="Search for..." aria-describedby="btnNavbarSearch" />
-            <button class="btn btn-primary" id="btnNavbarSearch" type="button"><i class="fas fa-search"></i></button>
+            {{-- <input class="form-control" type="text" placeholder="Search for..." aria-label="Search for..." aria-describedby="btnNavbarSearch" />
+            <button class="btn btn-primary" id="btnNavbarSearch" type="button"><i class="fas fa-search"></i></button> --}}
         </div>
     </form>
     <!-- Navbar-->
@@ -66,10 +66,7 @@
                         <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
                         Charts
                     </a>
-                    <a class="nav-link" href="tables.html">
-                        <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
-                        Tables
-                    </a>
+                   
                 </div>
             </div>
             <div class="sb-sidenav-footer">
