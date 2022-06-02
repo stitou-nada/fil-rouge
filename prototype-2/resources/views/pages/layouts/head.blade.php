5b3de39830e53038ca65caf2735e55c62c93f7ce
stitou-nada <nadastito1087@gmail.com>
2022-06-02 10:49:56 +0000

    edit page inserte

--- prototype-2/resources/views/pages/layouts/head.blade.php
+++ prototype-2/resources/views/pages/layouts/head.blade.php
@@ -6,8 +6,8 @@
     <meta name="author" content="" />
     <title>Static Navigation - SB Admin</title>
     
-    <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
     <link href="css/styles.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
 
     <link href="vendor/font-awesome-5/css/fontawesome-all.min.css" rel="stylesheet" media="all">
     <link href="vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
