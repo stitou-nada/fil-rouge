ec41fcdca08d7933d2faa3c4578f329bffb9c325
nada stitou <nadastito1087@gmail.com>
2022-06-14 23:14:11 +0100

    add

--- src/code-public/resources/views/pages/endroits.blade.php
+++ src/code-public/resources/views/pages/endroits.blade.php
@@ -99,7 +99,7 @@ foreach($places as $value){}
                                             class="icon material-icons-star_half"></span></div><a
                                         class="product-big-reviews" href="#">4 customer reviews</a>
                                 </div>
-                                <p class="product-big-text">{{$value->description_place}}</p><a class="button button-black-outline button-ujarak"
+                                <p class="product-big-text">{{$value->description_place}}...</p><a class="button button-black-outline button-ujarak"
                                     href="typography-1.html">plus d'informations</a>
                             </div>
                         </div>
